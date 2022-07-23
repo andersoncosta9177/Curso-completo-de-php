4ef043fabc44d286612f9e47272bc17880ba641e
andersoncosta9177 <anderson_costa_miguel@hotmail.com>
2022-07-22 21:53:10 -0300

    modulo operadores e expressoes

---
+++ 4_Expressões _e_Operadores/index.php
@@ -0,0 +1,123 @@
+<?php
+
+//ordem dos operadores;
+
+// echo 3+4 *5;
+// echo "<br>";
+// echo (3+3) * 5;
+// echo "<br>";
+// echo 5 + (2 / 10);
+// echo "<br>";
+
+// $a = 5;
+// $b = 2;
+// $c = 10;
+
+// echo $a + $b + $c;
+// echo "<br>";
+//  $d = $a * $b * $c;
+//   echo $d;
+
+// $a = 10;
+// $x = 5;
+// $z = 9;
+
+// $operacao = ($x - $z) /  $a * $z;
+// echo $operacao;
+
+//mudança de tipo implicito
+//  echo 5/3;
+//  echo "<br>";
+// if(is_float(5/2)){
+//     echo " 5/ 2 é  float<br>";
+
+// }
+
+// echo 2.3;
+// echo "<br>";
+//  if(is_string(2 . 3)){
+//     echo "Este e string<br>";
+//  }else{
+//     echo "Nao é string";
+//  }
+
+// $nome = "Miguel";
+// $sobrenome = 'costa';
+
+// $nomeCompleto = $nome. " ". $sobrenome;
+
+// echo $nomeCompleto;
+// echo "<br>";
+
+//exercicio
+
+// echo "5"  * 12;
+// echo 5 * "12";
+// echo "5" * "12";
+
+//operador modulo
+
+// $num = 11;
+
+// if($num % 2 == 0){
+//     echo "O numero $num é par";
+// }else{
+//     echo "O numero $num é impar";
+// }
+
+
+
+//operador exponenciaçao
+
+// echo 5 ** 2;
+// echo "<br>";
+// echo 9 ** 2;
+// echo "<br>";
+// echo 10 ** 2;
+
+// $nome = 'miguel';
+// $sobrenome = 'costa';
+// $idade =  21;
+// $profissao = 'Programador';
+//  echo "$nome  $sobrenome tem $idade anos de idade e sua profissão é $profissao";
+
+
+//Operadores de incremento e decremento
+
+// $x = 0;
+// $y =0;
+
+// $x++;
+// $x++;$x++;
+// echo $x;
+// echo "<br>";
+//  $y = $y + 1;
+//  echo $y;
+//  echo "<br>";
+
+
+// $num1 = 422;
+// $num2 = 44;
+//  if($num1 >= $num2){
+//     echo "verdade";
+//  }else{
+//     echo "False";
+//  }
+
+// $num1 = 411;
+// $num2 = 44;
+// if($num1 != $num2){
+//     echo "Verdade";
+// }else{
+//     echo "Falso";
+// }
+
+// $num1 = 44;
+// $num2 = 44;
+// if($num1 == $num2){
+//     echo "Verdade";
+// }else{
+//     echo "Falso";
+// }
+
+

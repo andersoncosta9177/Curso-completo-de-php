65ae0d5c093afc821a4f9746fa97cb5832292e13
andersoncosta9177 <anderson_costa_miguel@hotmail.com>
2022-07-22 22:28:23 -0300

    modulo estruturas de controle

---
+++ 5_estrutura_controle/index.php
@@ -0,0 +1,137 @@
+<?php
+
+// if(5 > 2){
+//     echo 'true<br>';
+// }else{
+//     echo "false<br>";
+// }
+
+// echo "====================================<br>";
+// if(5 >= 23){
+//     echo 'true<br>';
+// }else{
+//     echo "false<br>";
+// }
+// echo "====================================<br>";
+
+// if( 10 === 10 && 9 > 3){
+//     echo "True<br>";
+// }else{
+//     echo "Falsebr>";
+// }
+
+// echo "====================================<br>";
+
+// //exercicicios
+// if(5 > 2){
+//     echo "5 é maior que 2<br>";
+// }else{
+//     echo "5 nao é maior que 2<br>";
+// }
+
+
+// echo "====================================<br>";
+
+//  $nome  = "Miguel";
+//  $nome2 = "Miguel";
+
+//  if( $nome === $nome2){
+//     echo "Nomes sao iguais";
+//  }else{
+//     echo "Nomes nao sao iguais";
+//  }
+
+
+
+// $idade = 11;
+//   if($idade >= 18){
+//     echo "Maior de idade";
+//   }else{
+//     echo "Não é maior de idade";
+//   }
+
+//   echo "<br><hr>";
+
+// $idade = 16;
+// if($idade >= 18){
+//     echo "Voto obrigatorio";
+// }else if($idade >=16){
+//     echo "Pode votar, mas não é obrigatorio";
+// }else{
+//     echo "Não pode votar";
+// }
+
+
+// $peso = 112;
+// if($peso >= 80){
+//     echo "Esta pesado demais";
+
+// }else{
+//     echo "Peso dentro do limite";
+// }
+
+//if aninhado
+
+// if(10 > 2){
+//     echo "Entrou no primeiro if<br>";
+//     if("Teste" == "teste"){
+//         echo "entrou no segundo if";
+//     }
+// }
+
+// if(10 > 2){
+//     echo "Entrou no primeiro if<br>";
+//     if("teste" == "teste"){
+//         echo "entrou no segundo if";
+//     }else{
+//         echo "Entrou no else";
+//     }
+// }
+
+
+// $a = 22;
+// $b = 12;
+// $c = 'teste';
+
+// if(is_int($a) || is_float($a)){
+//     $mult = $a * 2;
+//     if($mult >= 100){
+//         echo "O numero e maior que 100<br>";
+//         echo $mult;
+//     }else{
+//         echo "Nao é maior que 100<br>";
+//         echo $mult;
+//     }
+
+// }else{
+//     echo 'Nao e um numero';
+// }
+
+
+//estrutura switch
+// $x = 8;
+// switch ($x) {
+//     case 0:
+//         echo "Domingo";
+//         break;
+//     case 1:
+//         echo "Segunda";
+//         break;
+//     case 2:
+//         echo "Terça";
+//         break;
+//     case 3:
+//         echo "Quarta";
+//         break;
+//     case 4:
+//         echo "Quinta";
+//         break;
+//     case 5:
+//         echo "sexta";
+//         break;
+//     case 6:
+//         echo "Sabado";
+//         break;
+//     default:
+//         echo "Valor invalido<br>Digite um valor entre 0 e 6";
+// }

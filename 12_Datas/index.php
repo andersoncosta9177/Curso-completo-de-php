04baa866b7311ece2b9a2361996c611f91fea715
andersoncosta9177 <anderson_costa_miguel@hotmail.com>
2022-07-27 00:05:14 -0300

    módulo Datas

---
+++ 12_Datas/index.php
@@ -0,0 +1,88 @@
+<?php
+
+// $data = date('d/m/y') ;//dia/mes/ano
+// $data2 = date('d, M  Y');
+// echo "<h1>$data<h1><br>";
+// echo "<h1>$data2<h1><br>";
+
+// $data3 = date('d/M/Y');
+// echo    $data3."<br>";
+
+// funçao mktime;
+// $datanasc = mktime(02,12,33, 02,05,1989);
+// echo $datanasc;
+// $dataFormatada = date('d/m/Y', $datanasc);
+// echo "<br>";
+// echo $dataFormatada;
+
+
+// $cindodias = strtotime("1 days");
+// $tenDays  = strtotime("10 days");
+
+// $dataAtual = date('d/m/y', $cindodias);
+// echo $dataAtual;
+// echo "<br>";
+// $dataAtual = date('d/m/y', $tenDays);
+// echo $dataAtual;
+
+//  $dataFutura = strtotime("12 years");
+//  $dataFormat  =  date('d/m/y',$dataFutura);
+//  echo $dataFormat; 
+
+
+
+// $data  = new DateTime();
+// print_r($data->date);
+
+
+// $data = new DateTime();
+// echo $data->format('d/m/y');
+// echo "<br>";
+// echo $data->format('h:m:s');
+// echo "<br>";
+// echo $data->format('D/M/Y');
+// echo "<br>";
+
+// echo "-----------------------<br>";
+// $data->modify("-5 days");
+// echo$data->format('d/m/y');
+// echo "<br>";
+
+// $dataA = new DateTime();
+// $dataB = new DateTime();
+
+// $dataB->setDate(2001,10,15);
+// print_r($dataA);
+// print_r($dataB);
+// echo "=========diferença=======<br>";
+// $diferença = $dataA->diff($dataB);
+// print_r($diferença);
+// echo "<br>";
+// echo $diferença->format("%a days");
+
+
+
+
+// $dataA = new DateTime();
+// $dataB = new DateTime();
+// $dataB->setDate(2008,10,04);
+
+// if($dataA > $dataB){
+//     echo "A data A  é maior que a data B";
+// }else{
+//     echo "A data é menor que B";
+// }
+
+// date_default_timezone_set('America/Sao_Paulo');
+// $data = new DateTime();
+// $data->format('h:m:s');
+// print_r($data);
+// echo "<br>";
+// print_r($data);
+// echo "<br>";
+
+
+
+
+
+?>
